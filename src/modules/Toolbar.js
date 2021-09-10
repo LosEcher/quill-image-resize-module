57616c4fbd8323fb021209512d7dcd86a8e3770a
los <kknkkk@hotmail.com>
2021-09-10 23:01:31 +0800

    Resolves the "Cannot read property 'imports' of undefined" problem.
    Resolves the "Cannot read property 'imports' of undefined" problem.
    
    https://github.com/kensnyder/quill-image-resize-module/pull/38

--- src/modules/Toolbar.js
+++ src/modules/Toolbar.js
@@ -2,8 +2,10 @@ import IconAlignLeft from 'quill/assets/icons/align-left.svg';
 import IconAlignCenter from 'quill/assets/icons/align-center.svg';
 import IconAlignRight from 'quill/assets/icons/align-right.svg';
 import { BaseModule } from './BaseModule';
+import VQuill from 'quill';
+const Quill = window.Quill || VQuill;
 
-const Parchment = window.Quill.imports.parchment;
+const Parchment = Quill.imports.parchment;
 const FloatStyle = new Parchment.Attributor.Style('float', 'float');
 const MarginStyle = new Parchment.Attributor.Style('margin', 'margin');
 const DisplayStyle = new Parchment.Attributor.Style('display', 'display');
